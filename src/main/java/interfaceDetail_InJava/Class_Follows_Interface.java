ed7e959fb2ae20c53732706a2a9c9468811ea937
Haridass-M <haridassm2809@gmail.com>
2024-11-08 10:41:15 +0530

    Interface follow another Interface Interface follow class

---
+++ src/main/java/interfaceDetail_InJava/Class_Follows_Interface.java
@@ -0,0 +1,53 @@
+package interfaceDetail_InJava;
+
+public class Class_Follows_Interface implements InterfaceA2_followsAnotherInterface {
+
+//	if any class follows any interface, whatever the inherited Abstract/Boby less method will be automatically create inside class
+	@Override
+	public void buyPen() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void buyEgg21() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void bugGift22() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void calling_MethodAndVariabe() {
+		System.out.println(InterfaceA1.b);
+	
+	Class_Follows_Interface obj = new Class_Follows_Interface();
+	 obj.buybike(); //calling default method from InterfaceA1 with object creation
+	 obj.buyPen(); //calling non-static method from class_follows_Interfce with object creation
+	 InterfaceA1.buySoap(); //calling static method from InterfaceA1 with Interface_Name
+	 
+	 InterfaceA2_followsAnotherInterface.buyCar23(); //calling static method from InterfaceA2followsInterface with Interface_Name
+	 obj.buyMobile24(); //calling default method from InterfaceA2followsInterface with object creation
+	 obj.buyEgg21(); //calling non-static method from class_follows_Interfce with object creation
+	 obj.bugGift22(); //calling non-static method from class_follows_Interfce with object creation
+	
+	}
+	public void applePhone() {
+		System.out.println("Apple showroom");
+		
+	}
+}
+
+class SuperMarket{
+	public static void main(String [] args) {
+		
+	
+		Class_Follows_Interface obj1 = new Class_Follows_Interface();
+		obj1.applePhone();
+		obj1.calling_MethodAndVariabe();
+	}
+	
+}
---
+++ src/main/java/interfaceDetail_InJava/InterfaceA1.java
@@ -0,0 +1,17 @@
+package interfaceDetail_InJava;
+
+public interface InterfaceA1 {
+	int a=20;
+	static char b='c';
+	
+	public void buyPen();
+
+	static public void buySoap() {
+		System.out.println("Static method from A1");
+	}
+	default int buybike() {
+		System.out.println("Non-Static method from A1");
+		return 0;
+	}
+
+}
