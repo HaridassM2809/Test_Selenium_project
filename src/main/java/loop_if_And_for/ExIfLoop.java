e9027d5bfd152c62787aabc314c0630f50abd537
Haridass-M <haridassm2809@gmail.com>
2024-11-19 10:34:51 +0530

    If loop practice

---
+++ src/main/java/loop_if_And_for/ExIfLoop.java
@@ -0,0 +1,26 @@
+package loop_if_And_for;
+
+public class ExIfLoop {
+	
+	int a =200;
+	String str ="HaRI";
+
+	public static void main(String[] args) {
+		
+		ExIfLoop obj = new ExIfLoop();
+		if(obj.a%2==0) {
+			System.out.println(obj.a+" is a even number");	
+		}
+		else
+		{
+			System.out.println(obj.a+" is a odd number");
+		}
+		
+		if(obj.str == "Hari") {
+			System.out.println("String was matching");
+		}
+		else {
+			System.out.println("String was not matching");
+		}
+	}
+}
