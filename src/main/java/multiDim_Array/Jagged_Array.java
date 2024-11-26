17408006f00bd98423b9f22bae522435e101eeff
Haridass-M <haridassm2809@gmail.com>
2024-11-26 11:07:32 +0530

    Jagged and Multidimensional Array with example

---
+++ src/main/java/multiDim_Array/Jagged_Array.java
@@ -0,0 +1,42 @@
+package multiDim_Array;
+
+public class Jagged_Array {
+	
+//	* Jagged array is nothing but, arrays of arrays but each arrays can have different column values
+//	* In Jagged array, length is nothing but total rows
+//	* We can not declare Jagged array as same as MultiDimentional array. We need to initialize jagged array while declaring itself.
+//		* Because in multiDimentional array, column size is same across the arrays
+//		* But in Jagged array, column size of each array may vary
+//	* We can print jagged array by using for loops only. 
+//	* We can not simply use sysout or Arrays class methods to print jagged array
+
+	public static void main(String[] args) {
+		
+		int a[]= {10,20,30,50};
+		int b[]= {21,31,42};
+		int c[]= {54,98};
+		
+		int d[][]= {a,b,c};
+		
+//		for (int[] each : d) {
+//			System.out.println(each);  // from this foreach loop we can't print the array value.
+//		}
+		
+		System.out.println("Jagged array length will be always row size of that array ");
+		System.out.println(d.length); //for jagged array row size is lenght ..,3
+		System.out.println(d[0].length); //4
+		System.out.println(d[2].length); //2
+		
+
+		System.out.println("To print Jagged array values using normal for loop");
+		for (int i=0;i<d.length;i++) {
+			for (int j=0;j<d[i].length;j++) {
+				System.out.println(d[i][j]);
+			}
+			
+		}
+		
+
+	}
+
+}
---
+++ src/main/java/multiDim_Array/MultiDim_Array.java
@@ -0,0 +1,46 @@
+package multiDim_Array;
+
+public class MultiDim_Array {
+	
+//	* Nested arrays are nothing but multi dimensional array
+//	* 2D array - array of arrays
+//	* 3D array - array of 2D arrays
+//	* In Multidimensional array, length is nothing but "row size".
+//	* To print multidimensional array, we rely on for loops. We can not use simple sysout and Arrays class methods
+//	* While printing multi-dimensional/jagged array using for loop, column size is rely on row of array
+
+	public static void main(String[] args) {
+		String str [][]= {{"Hari","Dass","Harish"},{"Raxlin","Sharala","RaxSha"},{"Avi","John","Jane"}};
+		
+//		or
+		
+//		int a1[]= {10,20,30};
+//		int a2[]= {11,22,33};
+//		int a3[]= {21,31,41};
+//		
+//		int a[][]= {a1,a2,a3};
+		
+//		for (String[] each: str) {
+//			System.out.println(each);    // we can't print array value using for each
+//		}
+		for (int i=0;i<=str.length-1;i++) {
+			for (int j=0;j<=str[i].length-1;j++) {
+				System.out.println(str[i][j]);
+				
+			}
+		}
+		
+		System.out.println("Print multi dim array using method");
+		MultiDim_Array obj = new MultiDim_Array();
+		obj.printMultiDimArray(str);
+		
+	}
+	public void printMultiDimArray(String[][] array) {
+		for (String[] eachArr : array) {
+			for (String eachVal : eachArr) {
+				System.out.println(eachVal);
+			}
+		}
+	}
+
+}
